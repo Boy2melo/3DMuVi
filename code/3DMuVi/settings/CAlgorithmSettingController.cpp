49fa0375b04d253c3f03e87cc4b51a51411eefee
Jens <spongepop@arcor.de>
2016-01-26 10:53:49 +0100

    Implementieren von AlgorithmSettingController, soweit wie ohne IO möglich.

--- code/3DMuVi/settings/CAlgorithmSettingController.cpp
+++ code/3DMuVi/settings/CAlgorithmSettingController.cpp
@@ -1,25 +1,64 @@
 #include "CAlgorithmSettingController.h"
+#include "QJsonDocument"
 
-CAlgorithmSettingController::CAlgorithmSettingController(QUrl diretory)
+CAlgorithmSettingController::CAlgorithmSettingController(QUrl directory)
 {
-
+    tempdirectory = QUrl(directory);
 }
 QJsonObject CAlgorithmSettingController::getSetting(QString name)
 {
-
+    QUrl url;
+    QString file;
+    if (algorithms.contains(name)) {
+       url = QUrl(tempdirectory.toString() + name);
+       //file = io.TextIo.load(url);
+       QJsonDocument docu = QJsonDocument().fromJson(file.toUtf8());
+       return docu.object();
+    }
 }
 
 bool CAlgorithmSettingController::setSetting(QString name, QJsonObject data)
 {
-
+    QJsonDocument docu;
+    QString file;
+    QUrl url;
+    if (!algorithms.contains (name)) {
+        algorithms.append(name);
+    }
+    if(true){//Algorithmen checks if parameters are legal
+        docu = QJsonDocument(data);
+        file = QString(docu.toJson());
+        url = QUrl(tempdirectory.toString() + name);
+        //io.TextIo.save(url, file);
+        return true;
+    } else {
+        return false;
+    }
 }
 
-void CAlgorithmSettingController::import(QUrl diretory, QString name)
-{
-
+void CAlgorithmSettingController::import(QUrl directory, QString name)
+{ 
+    QString file;
+    QUrl url;
+    if (!algorithms.contains (name)) {
+        algorithms.append(name);
+    }
+    url = QUrl(directory.toString() + name);
+    //file = io.TextIo.load(url);
+    url = QUrl(tempdirectory.toString() + name);
+    //io.TextIo.save(url, file);
 }
 
-void CAlgorithmSettingController::exportto(QUrl diretory)
+void CAlgorithmSettingController::exportto(QUrl directory)
 {
-
+    QString file;
+    QUrl url;
+    QString name;
+    for (int i = 0; i < algorithms.length(); i++) {
+        name = algorithms.value(i);
+        url = QUrl(tempdirectory.toString() + name);
+        //file = io.TextIo.load(url);
+        url = QUrl(directory.toString() + name);
+        //io.TextIo.save(url, file);
+    }
 }
--- code/3DMuVi/settings/CAlgorithmSettingController.h
+++ code/3DMuVi/settings/CAlgorithmSettingController.h
@@ -4,19 +4,46 @@
 #include <QString>
 #include <QJsonObject>
 #include <QUrl>
-
+#include <QStringList>
+/*!
+* Controller for the parameters of the algortihms
+* Author Jens Manig
+*/
 class CAlgorithmSettingController 
 {
+private:
+    QUrl tempdirectory;
+    QStringList algorithms;
 public:
-  CAlgorithmSettingController(QUrl diretory);
-  
-  QJsonObject getSetting(QString name);
-
-  bool setSetting(QString name, QJsonObject data);
-
-  void import(QUrl diretory, QString name);
-
-  void exportto(QUrl diretory);
+    /*!
+    * Constructor for the Controller
+    * @param directory temporal workdirectory
+	*/
+    CAlgorithmSettingController(QUrl directory);
+    /*!
+    * Returns the setting for a certain algorithm
+    * @param name name of the algorithm
+    * @return the setting of the algorithm
+    */
+    QJsonObject getSetting(QString name);
+    /*!
+    * sets a new setting for an algorithm
+    * @param name name of the algorithm
+    * @param data the parameters of the algorithm as QJsonObject
+    * @return true if data was a legal setting, else false
+    */
+    bool setSetting(QString name, QJsonObject data);
+    /*!
+    * impoert a setting for an algorithm as QJsonObject
+    *@param directory of the setting
+    *@param name name of the setting
+    */
+    void import(QUrl directory, QString name);
+    /*!
+    * exports all algorithmsettings to a directory
+    *@param directory where all seeting should go
+    */
+    void exportto(QUrl directory);
 };
 
 #endif //CALGORITHMSETTINGCONTROLLER_H
