8f5c53cafd920d6d09557eb37ffad90f377c50fb
Jens <spongepop@arcor.de>
2016-01-28 15:46:26 +0100

    Hinzufügen von zwei protected methoden in CAlgorithmSettingController und verbessern der Dokumentation in CGlobalSettingController

--- code/3DMuVi/settings/CAlgorithmSettingController.cpp
+++ code/3DMuVi/settings/CAlgorithmSettingController.cpp
@@ -5,6 +5,40 @@ CAlgorithmSettingController::CAlgorithmSettingController(QUrl directory)
 {
     tempdirectory = QUrl(directory);
 }
+QJsonValue getSetting(QString name, QString key)
+{
+    QUrl url;
+    QString file;
+    QJsonObject tempjson;
+    if (algorithms.contains(name)) {
+       url = QUrl(tempdirectory.toString() + name);
+       //file = io.TextIo.load(url);
+       QJsonDocument docu = QJsonDocument().fromJson(file.toUtf8());
+       tempjson = docu.object();
+    }
+    return tempjson.value(key);
+}
+
+bool setSetting(QString name,QString key, QJsonValue value)
+{
+    QJsonDocument docu;
+    QString file;
+    QUrl url;
+    QJsonObject tempjson;
+    if (!algorithms.contains (name)) {
+        return false;
+    }
+    url = QUrl(tempdirectory.toString() + name);
+    //file = io.TextIo.load(url);
+    docu = QJsonDocument().fromJson(file.toUtf8());
+    tempjson = docu.object();
+    tempjson.insert(key, value);
+    docu = QJsonDocument(tempjson);
+    file = QString(docu.toJson());
+    url = QUrl(tempdirectory.toString() + name);
+    //io.TextIo.save(url, file);
+}
+
 QJsonObject CAlgorithmSettingController::getSetting(QString name)
 {
     QUrl url;
--- code/3DMuVi/settings/CGlobalSettingController.h
+++ code/3DMuVi/settings/CGlobalSettingController.h
@@ -11,34 +11,34 @@ private:
     QJsonObject settings;
 public:
     /*!
-     * constructor for GlobalSettingController
+     * \constructor for GlobalSettingController
      * /
     CGlobalSettingController();
     /*!
-     * gets a certain global setting
-     * @param name name of the setting
-     * @return value of teh setting as QString
+     * \gets a certain global setting
+     * \param name name of the setting
+     * \return value of teh setting as QString
      * /
     QString getSetting(QString name);
     /*!
-     * sets a global setting
-     * @param name mane of the setting
-     * @param data value of the setting
+     * \sets a global setting
+     * \param name mane of the setting
+     * \param data value of the setting
      * /
     bool setSetting(QString name, QString data);
     /*!
-     * imports a globalconfig file from a directory, file must be in Json Format
-     * @param directory directoy of the file
-     * @param name name of the file
+     * \imports a globalconfig file from a directory, file must be in Json Format
+     * \param directory directoy of the file
+     * \param name name of the file
      * /
     void import(QUrl diretory, QString name);
     /*!
-     * exports the globalsettings in a config file in JsonFormat
-     * @param directory the directory where the config should be
+     * \exports the globalsettings in a config file in JsonFormat
+     * \param directory the directory where the config should be
      * /
     void exportto(QUrl diretory);
     /*!
-     * resets all settings to default
+     * \resets all settings to default
      * /
     void resettoDefault();
 };
