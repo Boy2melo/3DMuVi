df54832d9bdd40f8507f160ebec0a917c6df08bc
Stefan Wolf <stefan_wolf1@gmx.net>
2016-02-02 21:55:23 +0100

    GUI: CSettingsDialog implementiert.

--- code/3DMuVi/gui/CSettingsDialog.cpp
+++ code/3DMuVi/gui/CSettingsDialog.cpp
@@ -1,11 +1,11 @@
+#include <QFileDialog>
+#include <QMessageBox>
+
+#include <settings/CGlobalSettingController.h>
+
 #include "CSettingsDialog.h"
 #include "ui_CSettingsDialog.h"
 
-//============================================================
-/*!
-@param parent
-*/
-//============================================================
 CSettingsDialog::CSettingsDialog(QWidget *parent) :
   QDialog(parent),
   ui(new Ui::CSettingsDialog)
@@ -13,31 +13,28 @@ CSettingsDialog::CSettingsDialog(QWidget *parent) :
   ui->setupUi(this);
 }
 
-//============================================================
-/*!
-*/
-//============================================================
 CSettingsDialog::~CSettingsDialog()
 {
   delete ui;
 }
 
-
-//============================================================
-/*!
-*/
-//============================================================
 void CSettingsDialog::accept()
 {
+  CGlobalSettingController settings;
+  QString resultDirectory = ui->resultDirectoryEdit->text();
+  bool resultDirectorySuccess = settings.setSetting("resultDirectory", resultDirectory);
 
+  if(!resultDirectorySuccess)
+  {
+    QMessageBox::warning(this, "Warning", "Unable to set result directory.");
+  }
+
+  QDialog::accept();
 }
 
-//============================================================
-/*!
-@param checked
-*/
-//============================================================
-void CSettingsDialog::onResultDirectoryButtonClicked(bool checked)
+void CSettingsDialog::onResultDirectoryButtonClicked(bool)
 {
+  QUrl url = QFileDialog::getExistingDirectoryUrl(this, "Select result directory");
 
+  ui->resultDirectoryEdit->setText(url.toString());
 }
--- code/3DMuVi/gui/CSettingsDialog.h
+++ code/3DMuVi/gui/CSettingsDialog.h
@@ -7,16 +7,54 @@ namespace Ui {
   class CSettingsDialog;
 }
 
+/*!
+\brief A dialog for managing global settings.
+\author Stefan Wolf
+
+This class provides a dialog in which the user can change global settings.
+*/
 class CSettingsDialog : public QDialog
 {
   Q_OBJECT
 
 public:
+  //============================================================
+  /*!
+  \brief Initializes the dialog.
+
+  This constructor initializes the dialog.
+  @param parent The widget which should be the parent of this one.
+  */
+  //============================================================
   explicit CSettingsDialog(QWidget *parent = 0);
+
+  //============================================================
+  /*!
+  \brief Cleans up the dialog.
+
+  Destroys all created objects.
+  */
+  //============================================================
   ~CSettingsDialog();
 
 public slots:
+  //============================================================
+  /*!
+  \brief Applies the settings.
+
+  This method closes the dialog and applies the settings.
+  */
+  //============================================================
   void accept();
+
+  //============================================================
+  /*!
+  \brief Shows a directory selection dialog for the result directory.
+  Shows a directory selection dialog for the result directory.
+
+  @param checked Parameter will be ignored.
+  */
+  //============================================================
   void onResultDirectoryButtonClicked(bool checked);
 
 private:
