e1c447ef3cee32d7ccf85801bce79ee529ddefef
TimBrodb <timj.brodbeck2@googlemail.com>
2016-02-02 15:20:28 +0100

    Logger fully implemented and ready to use
    added qslogging files;
    fixxed Logger

--- code/3DMuVi/logger/controll/CAlgoMessage.cpp
+++ code/3DMuVi/logger/controll/CAlgoMessage.cpp
@@ -2,17 +2,11 @@
 #include "CLogController.h"
 #include <ctime>
 
-CAlgoMessage::CAlgoMessage(QString message, QString type = "INFO")
-{
-if( !((type == "ERROR") || (type == "WARNING") || (type == "DEBUG"))){
-        type = "INFO"
-}
-
-QString time = get_timestamp();
-CLogController::manageNewLogMessage(message, time, type);
-delete this;
-}
-QString get_timestamp(){
+/**
+ * @brief getTimestamp private function to create a timestamp
+ * @return the timestamp as QString
+ */
+QString getTimestamp(){
     QString res = "";
     time_t rawtime;
     struct tm * timeinfo;
@@ -23,3 +17,24 @@ QString get_timestamp(){
     res = buffer;
     return res;
 }
+/**
+ * @brief CAlgoMessage::CAlgoMessage constuctor of the class CAlgoMessage
+ *        to create a new log message
+ * @param message QString text to log
+ * @param clcontroll CLogController the message is send to
+ * @param type QString "ERROR", "WARNING", "DEBUG", "INFO" default value is "INFO"
+ *        if type is not one of this 4 types it will be set to "INFO"
+ */
+CAlgoMessage::CAlgoMessage(QString message,CLogController& clcontroll ,QString type)
+{
+if( !((type == "ERROR") || (type == "WARNING") || (type == "DEBUG")))
+{
+        type = "INFO";
+}
+
+QString time = "";
+time = getTimestamp();
+clcontroll.manageNewLogMessage(message, time, type);
+delete this;
+}
+
--- code/3DMuVi/logger/controll/CLogController.h
+++ code/3DMuVi/logger/controll/CLogController.h
@@ -1,20 +1,32 @@
 
 #ifndef _CLOGCONTROLLER_H
 #define _CLOGCONTROLLER_H
-
-class CLogController {
+#include "CLogHistory.h"
+#include <QUrl>
+#include <QObject>
+/**
+ * @brief The CLogController class this class controll the logger,
+ * manage the history and multiplexes the logmessages to the GUI (with singnal)
+ * add it to the loghistory and calls qslog to log it in a text file if a destination is set
+ */
+class CLogController
+         :public QObject
+{
+  Q_OBJECT
 public: 
     CLogController();
 
     void manageNewLogMessage(QString message, QString time, QString type);
     
-    void setHistory(CLogHistory h);
+    CLogHistory& getHistory();
     
     void setLog(QUrl dest);
     
     void closeLog();
 signals:
     void newLogMessage(QString message, QString time, QString type);
+
+
 };
 
 #endif //_CLOGCONTROLLER_H
