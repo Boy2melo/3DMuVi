0c99b048b3ffbec8caeac0ae57100e0cefeb023d
Stefan Wolf <stefan_wolf1@gmx.net>
2016-02-11 00:48:20 +0100

    Tests zu CMainWindow überarbeitet: -Tests laufen nun korrekt unter Ubuntu und anderen Plattformen -Hilfsslots werden nicht mehr als Test ausgeführt

--- code/3DMuVi/test/gui/CTestCMainWindow.cpp
+++ code/3DMuVi/test/gui/CTestCMainWindow.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include <QMessageBox>
 #include <QMenuBar>
 #include <QTimer>
@@ -18,28 +16,65 @@ void CTestCMainWindow::about()
 {
   CMainWindow mw;
 
+  openMenu("Help", &CTestCMainWindow::aboutClickAbout);
+
+  QVERIFY(mAboutSuccess);
+}
+
+void CTestCMainWindow::settings()
+{
+  CMainWindow mw;
+
+  openMenu("Options", &CTestCMainWindow::settingsClickSettings);
+
+  QVERIFY(mSettingsSuccess);
+}
+
+void CTestCMainWindow::workflowSelection()
+{
+  CMainWindow mw;
+  std::vector<QGroupBox*> algorithmBoxes;
+
+  openMenu("Workflow", &CTestCMainWindow::workflowSelectionClickFourPhase);
+
   for(QWidget* w : QApplication::allWidgets())
   {
-    QMenuBar* menuBar = qobject_cast<QMenuBar*>(w);
+    QGroupBox* groupBox = qobject_cast<QGroupBox*>(w);
 
-    if(menuBar)
+    if(groupBox)
     {
-      QTest::mouseClick(menuBar, Qt::LeftButton, 0, QPoint(200, 3));
+      algorithmBoxes.push_back(groupBox);
     }
   }
 
+  //needed to recalculate group boxes positions
+  mw.show();
+  QApplication::processEvents();
+
+  std::sort(algorithmBoxes.begin(), algorithmBoxes.end(),
+            [](QGroupBox* a, QGroupBox* b) -> bool
+  {
+    return a->y() < b->y();
+  });
+
+  workflowSelectionCheckGroupBox(algorithmBoxes.at(0), "Feature Matcher");
+  workflowSelectionCheckGroupBox(algorithmBoxes.at(1), "Depth Mapper");
+  workflowSelectionCheckGroupBox(algorithmBoxes.at(2), "Pose Estimator");
+  workflowSelectionCheckGroupBox(algorithmBoxes.at(3), "PCL Reconstructor");
+}
+
+void CTestCMainWindow::aboutClickAbout()
+{
   for(QWidget* w : QApplication::allWidgets())
   {
     QMenu* menu = qobject_cast<QMenu*>(w);
 
-    if(menu)
+    if(menu && menu->title() == "Help")
     {
       QTimer::singleShot(WAITING_TIME, this, &CTestCMainWindow::aboutCheckMessageBox);
       QTest::mouseClick(menu, Qt::LeftButton, 0, QPoint(3, 3));
     }
   }
-
-  QVERIFY(mAboutSuccess);
 }
 
 void CTestCMainWindow::aboutCheckMessageBox()
@@ -60,32 +95,18 @@ void CTestCMainWindow::aboutCheckMessageBox()
   }
 }
 
-void CTestCMainWindow::settings()
+void CTestCMainWindow::settingsClickSettings()
 {
-  CMainWindow mw;
-
-  for(QWidget* w : QApplication::allWidgets())
-  {
-    QMenuBar* menuBar = qobject_cast<QMenuBar*>(w);
-
-    if(menuBar)
-    {
-      QTest::mouseClick(menuBar, Qt::LeftButton, 0, QPoint(150, 3));
-    }
-  }
-
   for(QWidget* w : QApplication::allWidgets())
   {
     QMenu* menu = qobject_cast<QMenu*>(w);
 
-    if(menu)
+    if(menu && menu->title() == "Options")
     {
       QTimer::singleShot(WAITING_TIME, this, &CTestCMainWindow::settingsCheckDialog);
       QTest::mouseClick(menu, Qt::LeftButton, 0, QPoint(3, 3));
     }
   }
-
-  QVERIFY(mSettingsSuccess);
 }
 
 void CTestCMainWindow::settingsCheckDialog()
@@ -106,56 +127,40 @@ void CTestCMainWindow::settingsCheckDialog()
   }
 }
 
-void CTestCMainWindow::workflowSelection()
+void CTestCMainWindow::workflowSelectionClickFourPhase()
 {
-  CMainWindow mw;
-  std::vector<QGroupBox*> algorithmBoxes;
+  for(QWidget* w : QApplication::allWidgets())
+  {
+    QMenu* menu = qobject_cast<QMenu*>(w);
 
-  mw.show();
+    if(menu && menu->title() == "Workflow")
+    {
+      QTest::mouseClick(menu, Qt::LeftButton, 0, QPoint(3, 3));
+    }
+  }
+}
 
+template<typename Func>
+void CTestCMainWindow::openMenu(QString title, Func member)
+{
   for(QWidget* w : QApplication::allWidgets())
   {
     QMenuBar* menuBar = qobject_cast<QMenuBar*>(w);
 
     if(menuBar)
     {
-      QTest::mouseClick(menuBar, Qt::LeftButton, 0, QPoint(100, 3));
+      QList<QMenu*> menus = menuBar->findChildren<QMenu*>(QString(), Qt::FindDirectChildrenOnly);
+
+      for(QMenu* m : menus)
+      {
+        if(m->title() == title)
+        {
+          QTimer::singleShot(WAITING_TIME, this, member);
+          m->exec();
+        }
+      }
     }
   }
-
-  for(QWidget* w : QApplication::allWidgets())
-  {
-    QMenu* menu = qobject_cast<QMenu*>(w);
-
-    if(menu)
-    {
-      QTest::mouseClick(menu, Qt::LeftButton, 0, QPoint(3, 3));
-    }
-  }
-
-  for(QWidget* w : QApplication::allWidgets())
-  {
-    QGroupBox* groupBox = qobject_cast<QGroupBox*>(w);
-
-    if(groupBox)
-    {
-      algorithmBoxes.push_back(groupBox);
-    }
-  }
-
-  //needed to recalculate group boxes positions
-  QApplication::processEvents();
-
-  std::sort(algorithmBoxes.begin(), algorithmBoxes.end(),
-            [](QGroupBox* a, QGroupBox* b) -> bool
-  {
-    return a->y() < b->y();
-  });
-
-  workflowSelectionCheckGroupBox(algorithmBoxes.at(0), "Feature Matcher");
-  workflowSelectionCheckGroupBox(algorithmBoxes.at(1), "Depth Mapper");
-  workflowSelectionCheckGroupBox(algorithmBoxes.at(2), "Pose Estimator");
-  workflowSelectionCheckGroupBox(algorithmBoxes.at(3), "PCL Reconstructor");
 }
 
 void CTestCMainWindow::workflowSelectionCheckGroupBox(QGroupBox* groupBox, QString reference)
--- code/3DMuVi/test/gui/CTestCMainWindow.h
+++ code/3DMuVi/test/gui/CTestCMainWindow.h
@@ -23,27 +23,11 @@ private slots:
   void about();
 
   /*!
-  \brief Helper for the about test.
-
-  This is a helper for the about test, which checks if the message is shown and closes it.
-  It doesn't has to be executed as a test.
-   */
-  void aboutCheckMessageBox();
-
-  /*!
   \brief Test for settings function.
 
   This is a test for the settings dialog.
    */
   void settings();
-
-  /*!
-  \brief Helper for the settings test.
-
-  This is a helper for the settings test, which checks if the dialog is shown and closes it.
-  It doesn't has to be executed as a test.
-   */
-  void settingsCheckDialog();
 
   /*!
   \brief Test for workflow selection function.
@@ -54,7 +38,46 @@ private slots:
    */
   void workflowSelection();
 
+protected slots:
+  /*!
+  \brief Helper for the about test.
+
+  This is a helper for the about test, which clicks on the about entry in the helpt menu.
+   */
+  void aboutClickAbout();
+
+  /*!
+  \brief Helper for the about test.
+
+  This is a helper for the about test, which checks if the message is shown and closes it.
+   */
+  void aboutCheckMessageBox();
+
+  /*!
+  \brief Helper for the settings test.
+
+  This is a helper for the settings test, which clicks on the settings entry in the options menu.
+   */
+  void settingsClickSettings();
+
+  /*!
+  \brief Helper for the settings test.
+
+  This is a helper for the settings test, which checks if the dialog is shown and closes it.
+   */
+  void settingsCheckDialog();
+
+  /*!
+  \brief Helper for the workflow selection test.
+
+  This is a helper for the workflow selection test, which clicks on the four phase entry in the
+  workflow menu.
+   */
+  void workflowSelectionClickFourPhase();
+
 private:
+  template<typename Func>
+  void openMenu(QString title, Func member);
   void workflowSelectionCheckGroupBox(QGroupBox* groupBox, QString reference);
 
   bool mAboutSuccess = false;
