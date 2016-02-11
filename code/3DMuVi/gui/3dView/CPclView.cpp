ae2503cd44aa106d0b05b79c064b9c11b794547e
Stefan Wolf <stefan_wolf1@gmx.net>
2016-02-11 18:51:12 +0100

    Methoden von CPclView implementiert.

--- code/3DMuVi/gui/3dView/CPclView.cpp
+++ code/3DMuVi/gui/3dView/CPclView.cpp
@@ -1,11 +1,19 @@
+#include <QVBoxLayout>
+
 #include <vtkRenderWindow.h>
 
 #include "CPclView.h"
+
+#define CAMERA_SHAPE_N_VERTICES 5
+#define CAMERA_SHAPE_N_LINES 8
 
 CPclView::CPclView(QWidget *parent) : QWidget(parent)
 {
   mpQVtkWidget.reset(new QVTKWidget(this));
   mpPclVisualizer.reset(new pcl::visualization::PCLVisualizer("", false));
+
+  setLayout(new QVBoxLayout);
+  layout()->addWidget(mpQVtkWidget.get());
 
   mpQVtkWidget->SetRenderWindow(mpPclVisualizer->getRenderWindow());
   mpPclVisualizer->setupInteractor(mpQVtkWidget->GetInteractor(), mpQVtkWidget->GetRenderWindow());
@@ -19,65 +27,104 @@ CPclView::~CPclView()
 
 void CPclView::addPointCloud(const pcl::PointCloud<pcl::PointXYZRGB>& pointCloud, const QString& id)
 {
+  pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloudPtr(&pointCloud);
 
+  mpPclVisualizer->addPointCloud(cloudPtr, id.toStdString());
 }
 
 void CPclView::addPolygonMesh(const pcl::PolygonMesh& mesh, const QString& id)
 {
-
+  mpPclVisualizer->addPolygonMesh(mesh, id.toStdString());
 }
 
 void CPclView::addTextureMesh(const pcl::TextureMesh& mesh, const QString& id)
 {
-
+  mpPclVisualizer->addTextureMesh(mesh, id.toStdString());
 }
 
-void CPclView::addCameraMesh(const pcl::PointXYZ& position, const Eigen::Vector4f& rotation,
+void CPclView::addCameraMesh(const pcl::PointXYZ& position, const Eigen::Quaternionf& rotation,
                              const QString& id)
 {
+  Eigen::Vector3f cameraBasePos(position.x, position.y, position.z);
+  Eigen::Quaternionf quat = rotation.normalized();
+  Eigen::Matrix3f rotationMatrix = quat.toRotationMatrix();
+  Eigen::Vector3f cameraShapeVertices[CAMERA_SHAPE_N_VERTICES];
+  pcl::PointXYZ pclCameraPoints[CAMERA_SHAPE_N_VERTICES];
 
+  cameraShapeVertices[0] = Eigen::Vector3f( 0.f,  0.f, 0.f);
+  cameraShapeVertices[1] = Eigen::Vector3f(-1.f, -1.f, 1.f);
+  cameraShapeVertices[2] = Eigen::Vector3f(-1.f,  1.f, 1.f);
+  cameraShapeVertices[3] = Eigen::Vector3f( 1.f, -1.f, 1.f);
+  cameraShapeVertices[4] = Eigen::Vector3f( 1.f,  1.f, 1.f);
+
+  for(int i = 0; i < CAMERA_SHAPE_N_VERTICES; i++)
+  {
+    cameraShapeVertices[i] = rotationMatrix * cameraShapeVertices[i];
+    cameraShapeVertices[i] += cameraBasePos;
+
+    pclCameraPoints[i] = pcl::PointXYZ(cameraShapeVertices[i](0), cameraShapeVertices[i](1),
+                                       cameraShapeVertices[i](2));
+
+    std::cout << cameraShapeVertices[i] << std::endl;
+  }
+
+  mpPclVisualizer->addLine(pclCameraPoints[0], pclCameraPoints[1], id.toStdString() + "0");
+  mpPclVisualizer->addLine(pclCameraPoints[0], pclCameraPoints[2], id.toStdString() + "1");
+  mpPclVisualizer->addLine(pclCameraPoints[0], pclCameraPoints[3], id.toStdString() + "2");
+  mpPclVisualizer->addLine(pclCameraPoints[0], pclCameraPoints[4], id.toStdString() + "3");
+  mpPclVisualizer->addLine(pclCameraPoints[1], pclCameraPoints[2], id.toStdString() + "4");
+  mpPclVisualizer->addLine(pclCameraPoints[1], pclCameraPoints[3], id.toStdString() + "5");
+  mpPclVisualizer->addLine(pclCameraPoints[2], pclCameraPoints[4], id.toStdString() + "6");
+  mpPclVisualizer->addLine(pclCameraPoints[3], pclCameraPoints[4], id.toStdString() + "7");
 }
 
 void CPclView::removePointCloud(const QString& id)
 {
-
+  mpPclVisualizer->removePointCloud(id.toStdString());
 }
 
 void CPclView::removePolygonMesh(const QString& id)
 {
-
+  mpPclVisualizer->removePolygonMesh(id.toStdString());
 }
 
 void CPclView::removeTextureMesh(const QString& id)
 {
-
+  mpPclVisualizer->removeShape(id.toStdString());
 }
 
 void CPclView::removeCameraMesh(const QString& id)
 {
-
+  for(int i = 0; i < CAMERA_SHAPE_N_LINES; i++)
+  {
+    mpPclVisualizer->removeShape(id.toStdString() + std::to_string(i));
+  }
 }
 
 void CPclView::updatePointCloud(const pcl::PointCloud<pcl::PointXYZRGB>& pointCloud,
                                 const QString& id)
 {
+  pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloudPtr(&pointCloud);
 
+  mpPclVisualizer->updatePointCloud(cloudPtr, id.toStdString());
 }
 
 void CPclView::updatePolygonMesh(const pcl::PolygonMesh& mesh, const QString& id)
 {
-
+  mpPclVisualizer->updatePolygonMesh(mesh, id.toStdString());
 }
 
 void CPclView::updateTextureMesh(const pcl::TextureMesh& mesh, const QString& id)
 {
-
+  removeTextureMesh(id);
+  addTextureMesh(mesh, id);
 }
 
-void CPclView::updateCameraMesh(const pcl::PointXYZ& position, const Eigen::Vector4f& rotation,
-                                const QString& id)
+void CPclView::updateCameraMesh(const pcl::PointXYZ& position,
+                                const Eigen::Quaternionf& rotation, const QString& id)
 {
-
+  removeCameraMesh(id);
+  addCameraMesh(position, rotation, id);
 }
 
 void CPclView::showCoordinateSystem(bool state)
--- code/3DMuVi/gui/3dView/CPclView.h
+++ code/3DMuVi/gui/3dView/CPclView.h
@@ -20,23 +20,23 @@ public:
   virtual ~CPclView();
 
   void addPointCloud(const pcl::PointCloud<pcl::PointXYZRGB>& pointCloud,
-                     const QString& id = QString());
-  void addPolygonMesh(const pcl::PolygonMesh& mesh, const QString& id = QString());
-  void addTextureMesh(const pcl::TextureMesh& mesh, const QString& id = QString());
-  void addCameraMesh(const pcl::PointXYZ& position, const Eigen::Vector4f& rotation,
-                     const QString& id = QString());
+                     const QString& id = QString("cloud"));
+  void addPolygonMesh(const pcl::PolygonMesh& mesh, const QString& id = QString("polymesh"));
+  void addTextureMesh(const pcl::TextureMesh& mesh, const QString& id = QString("texmesh"));
+  void addCameraMesh(const pcl::PointXYZ& position, const Eigen::Quaternionf& rotation,
+                     const QString& id = QString("camera"));
 
-  void removePointCloud(const QString& id = QString());
-  void removePolygonMesh(const QString& id = QString());
-  void removeTextureMesh(const QString& id = QString());
-  void removeCameraMesh(const QString& id = QString());
+  void removePointCloud(const QString& id = QString("cloud"));
+  void removePolygonMesh(const QString& id = QString("polymesh"));
+  void removeTextureMesh(const QString& id = QString("texmesh"));
+  void removeCameraMesh(const QString& id = QString("camera"));
 
   void updatePointCloud(const pcl::PointCloud<pcl::PointXYZRGB>& pointCloud,
-                        const QString& id = QString());
-  void updatePolygonMesh(const pcl::PolygonMesh& mesh, const QString& id = QString());
-  void updateTextureMesh(const pcl::TextureMesh& mesh, const QString& id = QString());
-  void updateCameraMesh(const pcl::PointXYZ& position, const Eigen::Vector4f& rotation,
-                        const QString& id = QString());
+                        const QString& id = QString("cloud"));
+  void updatePolygonMesh(const pcl::PolygonMesh& mesh, const QString& id = QString("polymesh"));
+  void updateTextureMesh(const pcl::TextureMesh& mesh, const QString& id = QString("texmesh"));
+  void updateCameraMesh(const pcl::PointXYZ& position, const Eigen::Quaternionf& rotation,
+                        const QString& id = QString("camera"));
 
   void showCoordinateSystem(bool state);
   void setBackground(float red, float blue, float green);
