dbe098a714554d81e68c0b386d4e083f216e908c
Stefan Wolf <stefan_wolf1@gmx.net>
2016-02-19 22:59:20 +0100

    CDataFusion implementiert.

--- code/3DMuVi/workflow/workflow/datapackets/CDataFusion.h
+++ code/3DMuVi/workflow/workflow/datapackets/CDataFusion.h
@@ -1,11 +1,34 @@
 #ifndef CDATAFUSION_H
 #define CDATAFUSION_H
 
+#include <pcl/common/projection_matrix.h>
+#include <pcl/point_types.h>
 
-class CDataFusion
+#include "workflow/workflow/idatapacket.h"
+
+using PointCloud = pcl::PointCloud<pcl::PointXYZRGB>;
+
+/*!
+ * \class CDataFusion
+ * \brief The CDataFusion class
+ * \author Stefan Wolf
+ */
+class CDataFusion : public IDataPacket
 {
 public:
-    CDataFusion();
+  CDataFusion();
+  virtual ~CDataFusion();
+
+  QString getDataType() const override;
+  AStreamProvider* getStreamProvider() override;
+  void serialize(AStreamProvider* stream) override;
+  void deserialize(AStreamProvider *stream) override;
+  void setPointClound(PointCloud::Ptr cloud);
+  PointCloud::Ptr getPointCloud();
+
+private:
+  PointCloud::Ptr mPointCloudData;
+  AStreamProvider* mpStream;
 };
 
 #endif // CDATAFUSION_H
