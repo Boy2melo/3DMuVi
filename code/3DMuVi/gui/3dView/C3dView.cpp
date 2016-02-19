1dd2c35eeeb0b7300f18d759469ce2297c49b26f
Stefan Wolf <stefan_wolf1@gmx.net>
2016-02-20 00:56:36 +0100

    GUI: C3dView implementiert.

--- code/3DMuVi/gui/3dView/C3dView.cpp
+++ code/3DMuVi/gui/3dView/C3dView.cpp
@@ -1,51 +1,68 @@
 #include "C3dView.h"
 
-//============================================================
-/*!
-@param modelTypeComboBox
- */
-//============================================================
-//TODO: add combo box to select model type
-C3dView::C3dView() //QComboBox& modelTypeComboBox)
+C3dView::C3dView(QWidget* parent) : CPclView(parent)
 {
 
 }
 
-//============================================================
-/*!
- */
-//============================================================
+void C3dView::setModelTypeSelector(QComboBox* modelTypeComboBox)
+{
+  modelTypeComboBox->addItem("Point Cloud", QVariant(POINT_CLOUD));
+  modelTypeComboBox->addItem("Mesh", QVariant(MESH));
+  modelTypeComboBox->addItem("Textured", QVariant(TEXTURED));
+
+  connect(modelTypeComboBox, static_cast<void(QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
+          this, &C3dView::onCurrentIndexChangedModelType);
+}
+
 void C3dView::activate()
 {
+  std::vector<uint32_t> imageIds;
 
+  //TODO: Is the camera id the same as the image id? Why different types?
+  for(uint64_t id : mPoseIds)
+  {
+    imageIds.push_back(id);
+  }
+
+  emit relevantImagesChanged(imageIds);
 }
 
-//============================================================
-/*!
- * @param packet
- */
-//============================================================
-/*void C3dView::applyData(CPclDataPacket* packet)
+void C3dView::applyData(CDataFusion* packet)
 {
+  if(mCloudAdded)
+  {
+    updatePointCloud(*packet->getPointCloud());
+  }
+  else
+  {
+    addPointCloud(*packet->getPointCloud());
+    mCloudAdded = true;
+  }
+}
 
-}*/
-
-//============================================================
-/*!
- * @param packet
- */
-//============================================================
-/*void C3dView::applyData(CPoseDataPacket* packet)
+void C3dView::applyData(CDataPose* packet)
 {
+  std::vector<SPose> poses = packet->getPose();
 
-}*/
+  for(uint64_t id : mPoseIds)
+  {
+    removeCameraMesh("camera" + id);
+    mPoseIds.clear();
+  }
 
-//============================================================
-/*!
- * @param index
- * @return slot
- */
-//============================================================
+  for(SPose p : poses)
+  {
+    pcl::PointXYZ position(p.translation.x(), p.translation.y(), p.translation.z());
+    Eigen::Quaternionf rotation(p.orientation.scalar(), p.orientation.x(), p.orientation.y(),
+                       p.orientation.z());
+
+    addCameraMesh(position, rotation, "camera" + p.cameraId);
+    mPoseIds.push_back(p.cameraId);
+  }
+}
+
+//TODO: only point cloud available
 void C3dView::onCurrentIndexChangedModelType(int index)
 {
 }
--- code/3DMuVi/gui/3dView/C3dView.h
+++ code/3DMuVi/gui/3dView/C3dView.h
@@ -5,15 +5,27 @@
 #include <QComboBox>
 
 #include <workflow/workflow/idataview.h>
+#include <workflow/workflow/datapackets/CDataFusion.h>
+#include <workflow/workflow/datapackets/CDataPose.h>
 
 #include <gui/IGuiDataView.h>
 
 #include "CPclView.h"
 
+/*!
+\brief A widget presenting a point cloud and camera poses.
+\author Stefan Wolf
+
+This class is a widget which presents a point cloud and camera poses. It uses the pcl visualizer
+for drawing.
+*/
 class C3dView : public CPclView, public IGuiDataView, public IDataView
 {
   Q_OBJECT
 
+  /*!
+  \brief Defines possible types of model types to present
+  */
   enum E3dModelType
   {
     POINT_CLOUD,
@@ -22,15 +34,59 @@ class C3dView : public CPclView, public IGuiDataView, public IDataView
   };
 
 public:
-  //TODO: add combo box to select model type
-  C3dView(); //QComboBox& modelTypeComboBox);
-  void activate();
-  //TODO: Include  data packets
-  //void applyData(CPclDataPacket* packet);
-  //void applyData(CPoseDataPacket* packet);
+  /*!
+  \brief Initializes the object.
+  \param parent The parent of this widget.
+  */
+  C3dView(QWidget* parent = nullptr);
 
-public slots:
+  /*!
+  \brief Sets the combo box for the model type selection.
+  \param modelTypeComboBox A combo box to select the model type to show.
+
+  This method adds the possible model types to the combo box and connects it to this class so that
+  the selected model will be shown when the user changes the selection.
+  */
+  void setModelTypeSelector(QComboBox* modelTypeComboBox);
+
+  /*!
+  \brief Activates the widget.
+
+  This method sends out the signal relevantImagesChanged with the poses as image IDs.
+  */
+  void activate();
+
+  /*!
+  \brief Shows the given fusion data.
+  \param packet The data to show.
+
+  Displays the model which is in the given data packet.
+  */
+  void applyData(CDataFusion* packet);
+
+  /*!
+  \brief Shows the given pose data.
+  \param packet The data to show.
+
+  Displays the given poses as camera meshes.
+  */
+  void applyData(CDataPose* packet);
+
+signals:
+  /*!
+  \brief Emitted when activate is called.
+  \param images Relevant images IDs.
+
+  This signal indicates that a new set of images is relevant for the user now.
+  */
+  void relevantImagesChanged(std::vector<uint32_t>& images);
+
+private slots:
   void onCurrentIndexChangedModelType(int index);
+
+private:
+  bool mCloudAdded = false;
+  std::vector<uint64_t> mPoseIds;
 };
 
 #endif // C3DVIEW_H
