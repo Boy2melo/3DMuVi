9b2c5e558074f8b81ffba296977cfa4987376419
boitumeloruf <boitumelo.ruf@iosb.fraunhofer.de>
2016-02-23 17:05:49 +0100

    added depth fusor plugin

---
+++ code/3DMuVi Plugins/PluginDepthFusor/PluginDepthFusor.pro
@@ -0,0 +1,56 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-02-15T22:18:43
+#
+#-------------------------------------------------
+
+QT       += core gui widgets
+
+TARGET = PluginDepthFusion
+TEMPLATE = lib
+CONFIG += plugin
+
+DESTDIR = $$[QT_INSTALL_PLUGINS]/generic
+
+QMAKE_CXXFLAGS += -std=c++11
+QMAKE_CXXFLAGS_RELEASE += -O3
+
+# add suffix d to debug output
+CONFIG(debug, debug|release) {
+  win32: TARGET =  $$join(TARGET,,,d)
+  unix: TARGET = $$join(TARGET,,,_debug)
+}
+
+SOURCES += \
+    plugin.cpp \
+    algorithm.cpp \
+    MyAlgorithm.cpp
+
+INCLUDEPATH += ../../3DMuVi
+
+DEFINES += BUILD_NAME=\\\"plugins/$${TARGET}\\\"
+
+HEADERS += \
+    ../../3DMuVi/workflow/plugin/ialgorithm.h \
+    ../../3DMuVi/workflow/plugin/iplugin.h \
+    plugin.h \
+    algorithm.h \
+    plugin_config.h
+OTHER_FILES += \
+    Plugin.json
+
+unix {
+    target.path = /usr/lib
+    INSTALLS += target
+}
+
+e################
+# PCL
+unix {
+    INCLUDEPATH += /home/rufboi/3rd_party/PCL_1.7.2_gcc4.8/include/
+    DEPENDPATH += /home/rufboi/3rd_party/PCL_1.7.2_gcc4.8/include/
+
+    LIBS += -L/home/rufboi/3rd_party/PCL_1.7.2_gcc4.8/lib/
+    LIBS += -lpcl_common
+    LIBS += -lpcl_io
+}
---
+++ code/3DMuVi Plugins/PluginDepthFusor/plugin.cpp
@@ -0,0 +1,65 @@
+#include "plugin.h"
+#include "workflow/plugin/cpluginmanager.h"
+#include <QPluginLoader>
+#include <QJsonObject>
+
+//----------------------------------------------
+// Autofunctions, no adjustments needed
+//----------------------------------------------
+QString _CLASS_GEN(Plugin)::Autor() const {
+    return str(_PLUGIN_AUTHOR);
+}
+
+qint32 _CLASS_GEN(Plugin)::Version() const {
+    return _PLUGIN_VERSION;
+}
+
+QDate _CLASS_GEN(Plugin)::Date() const {
+    return QDate::fromString(str(_PLUGIN_DATE), "yyyy-MM-dd");
+}
+
+QString _CLASS_GEN(Plugin)::Name() const {
+    return str(_PLUGIN_NAME);
+}
+
+QJsonObject _CLASS_GEN(Plugin)::GetParameterJson() const {
+    return mParameters.value("Default").toObject();
+}
+
+QJsonObject _CLASS_GEN(Plugin)::GetParameterDescriptionJson() const {
+    return mParameters.value("Description").toObject();
+}
+
+IAlgorithm *_CLASS_GEN(Plugin)::getAlgorithm() const {
+    return static_cast<IAlgorithm*>(mAlgorithm);
+}
+
+_CLASS_GEN(Plugin)::~_CLASS_GEN(Plugin)() {
+    delete mAlgorithm;
+}
+
+bool _CLASS_GEN(Plugin)::ValidateParameters(QJsonObject *params) const {
+    // First level type check over given parameters
+    for(QJsonObject::iterator itr = params->begin(); itr != params->end(); itr++){
+        // Get reference type
+        auto ref = GetParameterJson().value(itr.key());
+
+        if(ref.type() != itr.value().type()) {
+            return false;
+        }
+    }
+
+    return mAlgorithm->ValidateParameters(params);
+}
+
+QString _CLASS_GEN(Plugin)::GetPluginType() const {
+    return _PLUGIN_TYPE;
+}
+
+void _CLASS_GEN(Plugin)::Initialize(QPluginLoader* loader) {
+    mParameters = loader->metaData().value("MetaData").toObject();
+}
+
+_CLASS_GEN(Plugin)::_CLASS_GEN(Plugin)(){
+    mAlgorithm = new _CLASS_GEN(Algorithm)();
+}
