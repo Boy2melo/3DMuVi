54dda97f2421c913abd1313eae38a29c920e5eb6
Stefan Wolf <stefan_wolf1@gmx.net>
2016-02-29 19:32:55 +0100

    GUI: Dokumentation zu IGuiDataView und CPclView hinzugefügt.

--- code/3DMuVi/gui/3dView/CPclView.h
+++ code/3DMuVi/gui/3dView/CPclView.h
@@ -11,40 +11,136 @@
 #include <pcl/TextureMesh.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
+/*!
+\brief This class gives a simple interface for viewing 3D objects.
+\author Stefan Wolf
+*/
 class CPclView : public QWidget
 {
   Q_OBJECT
 
 public:
+  /*!
+  \brief Default constructor.
+  \param parent The widget which should be the parent of this one.
+  */
   explicit CPclView(QWidget *parent = nullptr);
+
+  /*!
+   * \brief Default destructor.
+   */
   virtual ~CPclView();
 
+  /*!
+   * \brief Adds a point cloud to the view.
+   * \param pointCloud The point cloud to add.
+   * \param id The id which is later used to update or remove the point cloud.
+   */
   void addPointCloud(const pcl::PointCloud<pcl::PointXYZRGB>& pointCloud,
                      const QString& id = QString("cloud"));
+
+  /*!
+   * \brief Adds a polygon mesh to the view.
+   * \param mesh The polygon mesh to add.
+   * \param id The id which is later used to update or remove the polygon mesh.
+   */
   void addPolygonMesh(const pcl::PolygonMesh& mesh, const QString& id = QString("polymesh"));
+
+  /*!
+   * \brief Adds a texture mesh to the view.
+   * \param mesh The texture mesh to add.
+   * \param id The id which is later used to update or remove the texture mesh.
+   */
   void addTextureMesh(const pcl::TextureMesh& mesh, const QString& id = QString("texmesh"));
+
+  /*!
+   * \brief Adds a camera mesh to the view.
+   * \param position The camera's position.
+   * \param rotation The camera's view angle.
+   * \param id The id which is later used to update or remove the camera.
+   */
   void addCameraMesh(const pcl::PointXYZ& position, const Eigen::Quaternionf& rotation,
                      const QString& id = QString("camera"));
 
+  /*!
+   * \brief Removes the point cloud with the given id from the view.
+   * \param id The point cloud's id.
+   */
   void removePointCloud(const QString& id = QString("cloud"));
+
+  /*!
+   * \brief Removes the polygon mesh with the given id from the view.
+   * \param id The polygon mesh's id.
+   */
   void removePolygonMesh(const QString& id = QString("polymesh"));
+
+  /*!
+   * \brief Removes the texture mesh with the given id from the view.
+   * \param id The texture mesh's id.
+   */
   void removeTextureMesh(const QString& id = QString("texmesh"));
+
+  /*!
+   * \brief Removes the camera mesh with the given id from the view.
+   * \param id The camera's id.
+   */
   void removeCameraMesh(const QString& id = QString("camera"));
 
+  /*!
+   * \brief Updates the point cloud with the given id.
+   * \param pointCloud The new point cloud.
+   * \param id The id of the point cloud which should be updated.
+   */
   void updatePointCloud(const pcl::PointCloud<pcl::PointXYZRGB>& pointCloud,
                         const QString& id = QString("cloud"));
+
+  /*!
+   * \brief Updates the polygon mesh with the given id.
+   * \param mesh The new polygon mesh.
+   * \param id The id of the polygon mesh which should be updated.
+   */
   void updatePolygonMesh(const pcl::PolygonMesh& mesh, const QString& id = QString("polymesh"));
+
+  /*!
+   * \brief Updates the texture mesh with the given id.
+   * \param mesh The new texture mesh.
+   * \param id The id of the texture mesh which should be updated.
+   */
   void updateTextureMesh(const pcl::TextureMesh& mesh, const QString& id = QString("texmesh"));
+
+  /*!
+   * \brief Updates the camera mesh with the given id.
+   * \param position The new camera's position.
+   * \param rotation The new camera's view angle.
+   * \param id The id of the camera which should be updated.
+   */
   void updateCameraMesh(const pcl::PointXYZ& position, const Eigen::Quaternionf& rotation,
                         const QString& id = QString("camera"));
 
+  /*!
+  \brief Not implemented.
+  */
   void showCoordinateSystem(bool state);
+
+  /*!
+  \brief Not implemented.
+  */
   void setBackground(float red, float blue, float green);
 
+  /*!
+  \brief Not implemented.
+  */
   void centerOnPoint(const pcl::PointXYZ& point);
 
 signals:
+  /*!
+  \brief Not implemented.
+  */
   void pointSelected(const pcl::PointXYZ& point);
+
+  /*!
+  \brief Not implemented.
+  */
   void cameraSelected(const pcl::PointXYZ& position, const Eigen::Vector4f& rotation);
 
 private:
--- code/3DMuVi/gui/IGuiDataView.h
+++ code/3DMuVi/gui/IGuiDataView.h
@@ -1,9 +1,23 @@
 #ifndef IGUIDATAVIEW_H
 #define IGUIDATAVIEW_H
 
+/*!
+\brief Interface for data views in the GUI.
+\author Stefan Wolf
+
+This interface schould be implemented by all views in the GUI to get a consistent way to access
+them.
+*/
 class IGuiDataView
 {
-public: 
+public:
+  /*!
+  \brief Called when data view is activated.
+
+  This method will be called when the data view gets visible. Implementing classes should then
+  do all necessary tasks to get the program in a correct state. Especially, a signal should be sent
+  indicating which images are relevant now.
+  */
   virtual void activate() = 0;
 };
 
