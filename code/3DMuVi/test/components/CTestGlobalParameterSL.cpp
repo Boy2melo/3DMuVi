d42d321e8864a582ddaffa8cc0a9cab6dc9a25a9
TimBrodb <timj.brodbeck2@googlemail.com>
2016-03-08 20:00:51 +0100

    Bugfix im Test übernommen

--- code/3DMuVi/test/components/CTestGlobalParameterSL.cpp
+++ code/3DMuVi/test/components/CTestGlobalParameterSL.cpp
@@ -9,7 +9,7 @@ QString test3 = "0";
 QString test4 = "1";
 
 cgsc.setSetting("resultDirectory",test1);
-cgsc.setSetting("minLogKevel",test2);
+cgsc.setSetting("minLogLevel",test2);
 cgsc.setSetting("logWindowEnabled",test3);
 cgsc.setSetting("logDataEnabled",test4);
 QUrl dir("/results");
@@ -18,7 +18,7 @@ cgsc.resetToDefault();
 cgsc.import(dir,"globalconfig");
 
 QCOMPARE(cgsc.getSetting("resultDirectory"),test1);
-QCOMPARE(cgsc.getSetting("minLogKevel"),test2);
+QCOMPARE(cgsc.getSetting("minLogLevel"),test2);
 QCOMPARE(cgsc.getSetting("logWindowEnabled"),test3);
 QCOMPARE(cgsc.getSetting("logDataEnabled"),test4);
 }
