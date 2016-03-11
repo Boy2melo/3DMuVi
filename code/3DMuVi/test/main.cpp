e0e748cacf367519783e3af7e927c34c06970e94
Stefan Wolf <stefan_wolf1@gmx.net>
2016-03-11 15:31:52 +0100

    Neue Tests zu Test-Suit hinzugefügt.

--- code/3DMuVi/test/main.cpp
+++ code/3DMuVi/test/main.cpp
@@ -3,20 +3,27 @@
 #include "gui/CTestCLogWidget.h"
 #include "gui/CTestCMainWindow.h"
 #include "gui/CTestCSettingsDialog.h"
+
 #include "io/CTestCImageIo.h"
 #include "io/CTestCTextIo.h"
 #include "io/CTestCInputDataSet.h"
 #include "io/CTestCResultContext.h"
+
 #include "logger/CTestLoggerControll.h"
 #include "logger/CTestLoggerHistory.h"
-#include "components/CTestAlgorithmSettings.h"
+
 #include "settings/CTestCAlgorithmSettingController.h"
-#include "workflow/plugin/CTestCPluginmanager.h"
-#include "components/CTestAlgorithmExecution.h"
 #include "settings/CTestCGlobalSettingController.h"
+
+#include "workflow/plugin/CTestCPluginmanager.h"
 #include "workflow/CTestCWorkflowmanager.h"
 #include "workflow/CTestCContextDataStore.h"
 #include "workflow/CTestCWorkflow.h"
+
+#include "components/CTestAlgorithmSettings.h"
+#include "components/CTestAlgorithmExecution.h"
+#include "components/CTestGlobalParameterSL.h"
+#include "components/CTestResultDir.h"
 
 
 int main(int argc, char* argv[])
@@ -24,15 +31,16 @@ int main(int argc, char* argv[])
   CTestCLogWidget logWidget;
   CTestCMainWindow mainWindow;
   CTestCSettingsDialog settingsDialog;
+
   CTestCImageIo imageIo;
   CTestCTextIo textIo;
   CTestCInputDataSet inputDataSet;
   CTestCResultContext resultContext;
+
   CTestLoggerControll logControll;
   CTestLoggerHistory logHistory;
-  CTestAlgorithmSettings algorithmSettings;
+
   CTestCAlgorithmSettingController algorithmcontroller;
-  CTestAlgorithmExecution algorithmExecution;
   CTestCGlobalSettingController globalcontroller;
 
   CTestCPluginmanager pluginmanager;
@@ -40,27 +48,39 @@ int main(int argc, char* argv[])
   CTestCContextDataStore datastore;
   CTestCWorkflow workflow;
 
+  CTestAlgorithmSettings algorithmSettings;
+  CTestAlgorithmExecution algorithmExecution;
+  CTestGlobalParameterSL globalParameterSL;
+  CTestResultDir resultDir;
+
   QApplication app(argc, argv);
 
   int failCounter = 0;
   failCounter += QTest::qExec(&logWidget, argc, argv);
   failCounter += QTest::qExec(&mainWindow, argc, argv);
   failCounter += QTest::qExec(&settingsDialog, argc, argv);
+
   failCounter += QTest::qExec(&imageIo, argc, argv);
   failCounter += QTest::qExec(&textIo, argc, argv);
   failCounter += QTest::qExec(&inputDataSet, argc, argv);
   //failCounter += QTest::qExec(&resultContext, argc, argv); Fehler in Datapackets führt zu einem seg fault.
+
   failCounter += QTest::qExec(&logControll, argc, argv);
   failCounter += QTest::qExec(&logHistory, argc, argv);
-  failCounter += QTest::qExec(&algorithmSettings, argc, argv);
+
   failCounter += QTest::qExec(&algorithmcontroller, argc, argv);
-  failCounter += QTest::qExec(&algorithmExecution, argc, argv);
+  failCounter += QTest::qExec(&globalcontroller, argc, argv);
+
   failCounter += QTest::qExec(&pluginmanager, argc, argv);
   failCounter += QTest::qExec(&workflowmanager, argc, argv);
   failCounter += QTest::qExec(&datastore, argc, argv);
   failCounter += QTest::qExec(&workflow, argc, argv);
 
+  failCounter += QTest::qExec(&algorithmSettings, argc, argv);
+  failCounter += QTest::qExec(&algorithmExecution, argc, argv);
+  failCounter += QTest::qExec(&globalParameterSL, argc, argv);
+  //failCounter += QTest::qExec(&resultDir, argc, argv); Fehler in Datapackets führt zu einem seg fault.
+
   std::cout << "Total number of fails: " << failCounter << "\n";
-  system("pause");
   return 0;  
 }
