c32e44c2619f179d5fefb591d3b792ff0d111a6c
Stefan Wolf <stefan_wolf1@gmx.net>
2016-03-20 13:32:10 +0100

    GUI: CDatasetSelector: Namen der Datasets sind nun änderbar.

--- code/3DMuVi/gui/CDatasetSelectorItem.cpp
+++ code/3DMuVi/gui/CDatasetSelectorItem.cpp
@@ -3,6 +3,7 @@
 CDatasetSelectorItem::CDatasetSelectorItem(CContextDataStore* dataStore, QString text)
   : QListWidgetItem(text), mpDataStore(dataStore)
 {
+  setFlags(flags() | Qt::ItemIsEditable);
 }
 
 CContextDataStore* CDatasetSelectorItem::getDataStore()
